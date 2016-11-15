e025094bb4f36c9faac867ae67a7d4c4d3836216
andy <448313485@qq.com>
2016-11-15 15:58:32 +0800

    完成图片位置定位

--- src/components/GalleryByReactApp.js
+++ src/components/GalleryByReactApp.js
@@ -1,29 +1,197 @@
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 
 //css
 // require("normalize.css");
 require('../styles/main.scss');
 
-// var imageDatas = require('../data/imagesDatas.json');
+var imageDatas = require('json!../data/imagesDatas.json');
+imageDatas = (function genImageUrl(imageDatasArr){
+  for(var i = 0 , j = imageDatasArr.length;i < j; i++){
+    var singleImageData = imageDatasArr[i];
+    singleImageData.imageURL = require('../images/'+singleImageData.fileName);
+    imageDatasArr[i] = singleImageData;
+  }
+  return imageDatasArr;
+})(imageDatas);
 
-// imageDatas = (function genImageUrl(imageDatasArr){
-//   for(var i = 0 , j = imageDatasArr.length;i < j; i++){
-//     var singleImageData = imageDatasArr[i];
-//     singleImageData.imageURL = require('../images/'+singleImageData.fileName);
-//     imageDatasArr[i] = singleImageData;
-//   }
-//   return imageDatasArr;
-// })(imageDatas);
+
+var ImgFigure = React.createClass({
+  render: function(){
+
+    var styleObj = {};
+    // 如果props属性中制定了这种图片的位置，则使用
+    if(this.props.arrange.pos){
+      styleObj = this.props.arrange.pos;
+    }
+
+    return (
+      <figure className = "img-figure" style = {styleObj}>
+        <img src={this.props.data.imageURL} alt={this.props.data.imageURL}/>
+        <figcaption>
+          <h2 className = "img-title">{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    )
+  }
+});
+
+/*
+  获取区间内的一个随机值
+*/
+function getRangeRandom(low,high){
+    return Math.ceil((Math.random() * (high - low) + low));
+}
 
 var GalleryByReactApp = React.createClass({
+  Constant: {
+    centerPos: {
+      left: 0,
+      right: 0
+    },
+    hPosRange: {  // 水平反向取值范围
+      leftSecX: [0,0],
+      rightSecX: [0,0],
+      y:[0,0]
+    },
+    vPosRange: { // 垂直方向的取值范围
+      x: [0,0],
+      topY: [0,0]
+    }
+  },
+  //重新布局图片
+  rearrange: function(centerIndex){
+      var imgsArrangeArr = this.state.imgsArrangeArr,
+          Constant = this.Constant,
+          centerPos = Constant.centerPos,
+          hPosRange = Constant.hPosRange,
+          vPosRange = Constant.vPosRange,
+          hPosRangeLeftSecX = hPosRange.leftSecX,
+          hPosRangeRightSecX = hPosRange.rightSecX,
+          hPosRangeY = hPosRange.y,
+          vPosRangeTopY = vPosRange.topY,
+          vPosRangeX = vPosRange.x,
+
+          imgsArrangeTopArr  = [],
+          topImgNum = Math.ceil(Math.random() * 2),//取零个或一个
+
+          topImgSpliceIndex = 0 ,
+          imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex,1);
+          // 首先居中 centerIndex 的图片
+          imgsArrangeCenterArr[0].pos = centerPos;
+
+          // 取出要布局上册的图片状态信息
+          topImgSpliceIndex = Math.ceil(Math.random() * (imgsArrangeArr.length - topImgNum));
+
+          imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex,topImgNum);
+
+          //布局位于上册的图片
+          imgsArrangeTopArr.forEach(function(value,index){
+            imgsArrangeTopArr[index].pos =  {
+              top: getRangeRandom(vPosRangeTopY[0],vPosRangeTopY[1]),
+              left: getRangeRandom(vPosRangeX[0],vPosRangeX[1])
+            }
+          });
+
+          //布局左右两侧的图片
+          for(var i = 0 , j = imgsArrangeArr.length, k = j /2 ; i < j ; i++){
+            var hPosRangeLORX = null;
+            // 前半部分布局左边，后半部分布局右边
+            if(i < k){
+              hPosRangeLORX = hPosRangeLeftSecX;
+            } else {
+              hPosRangeLORX = hPosRangeRightSecX;
+            }
+            imgsArrangeArr[i].pos =  {
+              top: getRangeRandom(hPosRangeY[0],hPosRangeY[1]),
+              left: getRangeRandom(hPosRangeLORX[0],hPosRangeLORX[1])
+            }
+          }
+
+          //重新组合数组
+          if(imgsArrangeTopArr && imgsArrangeTopArr[0]){
+            imgsArrangeArr.splice(topImgSpliceIndex,0,imgsArrangeTopArr);
+          }
+
+          imgsArrangeArr.splice(centerIndex,0,imgsArrangeCenterArr[0]);
+          // alert(this.state.imgsArrangeArr[0].pos.left);
+          this.setState({
+            imgsArrangeArr: imgsArrangeArr
+          });
+
+  },
+  getInitialState: function(){
+    return {
+      imgsArrangeArr: [
+        {
+          pos: {
+            left: '0',
+            top : '0'
+          }
+        }
+      ]
+    }
+  },
+  //组件加载后，为每张图片计算其位置的范围
+  componentDidMount: function(){
+    // 拿到舞台的大小
+    var stageDom = ReactDOM.findDOMNode(this.refs.stage),
+        stageW =  stageDom.scrollWidth,
+        stageH = stageDom.scrollHeight,
+        halfStageW = stageW/2,
+        halfStageH = stageH/2;
+
+    // 拿到一个imageFigure的大小
+    var imgFigureDom = ReactDOM.findDOMNode(this.refs.imgFigure0),
+        imgW = imgFigureDom.scrollWidth,
+        imgH = imgFigureDom.scrollHeight,
+        halfImgW = imgW/2,
+        halfImgH = imgH/2;
+
+    // 计算中心图片的位置点
+    this.Constant.centerPos = {
+      left: halfStageW - halfImgW,
+      top: halfStageH - halfImgH
+    }
+
+   //计算左侧，右侧区域图片排布位置的取值范围
+   this.Constant.hPosRange.leftSecX[0] = -halfImgW;
+   this.Constant.hPosRange.leftSecX[1] = halfImgW - 3*halfImgW;
+   this.Constant.hPosRange.rightSecX[0] = halfStageW + halfImgW;
+   this.Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
+   this.Constant.hPosRange.y[0] = -halfImgH;
+   this.Constant.hPosRange.y[1] = stageH - halfImgH;
+
+   this.Constant.vPosRange.topY[0] = -halfImgH;
+   this.Constant.vPosRange.topY[1] = halfStageH - halfImgH * 3;
+   this.Constant.vPosRange.x[0] = halfStageW - imgW;
+   this.Constant.vPosRange.x[1] = halfStageW;
+
+   this.rearrange(0);
+  },
   render:function(){
+    var controllerUnits = [],
+        imgFigures = [];
+    imageDatas.forEach(function(value,index){
+      if(!this.state.imgsArrangeArr[index]){
+        this.state.imgsArrangeArr[index] = {
+          pos: {
+            left: 0,
+            top: 0
+          }
+        }
+      }
+      imgFigures.push(<ImgFigure data = {value} ref ={'imgFigure' + index} arrange = {this.state.imgsArrangeArr[index]}/>);
+    }.bind(this));
     return (
-      <section className = "stage">
+      <section className = "stage" ref = "stage">
           <section className = "img-sec">
+            {imgFigures}
           </section>
           <nav className = "controller-nav">
+            {controllerUnits}
           </nav>
       </section>
     );
